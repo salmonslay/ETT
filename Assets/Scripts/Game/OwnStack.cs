e32bb30fecbf3967acee95264f229f8315ae3645
Fabian <lindgrenfabian@gmail.com>
2020-10-26 02:25:21 +0100

    Create script for own stacks

---
+++ Assets/Scripts/Game/OwnStack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwnStack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ToggleFocus(bool show)
+    {
+        GetComponent<Animation>().Play(show ? "PutForwards" : "PutAway");
+    }
+}
